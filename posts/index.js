94e899e1cdc0367f0e24c3b1b95a8d4b85fe1ae7
fgarcia <consultant-fgarcia@remine.com>
2024-02-12 15:16:08 -0600

    emmit event from post

--- posts/index.js
+++ posts/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,16 +13,26 @@ app.get('/posts', (req, res) => {
     res.send(posts);
 });
 
-app.post('/posts', (req, res) => {
-    const id = randomBytes(4).toString('hex');
-    const { title } = req.body;
+app.post('/posts', async (req, res) => {
+    try {
+        const id = randomBytes(4).toString('hex');
+        const { title } = req.body;
 
-    posts[id] = {
-        id,
-        title,
-    };
-
-    res.status(201).send(posts[id]);
+        posts[id] = {
+            id,
+            title,
+        };
+        await axios.post('http://localhost:4005/events', {
+            type: 'PostCreated',
+            data: {
+                id,
+                title,
+            },
+        });
+        res.status(201).send(posts[id]);
+    } catch (err) {
+        console.log('err', err);
+    }
 });
 
 app.listen(4000, () => {
